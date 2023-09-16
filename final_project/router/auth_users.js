3e3f0be2478b0c29df7bf48d56c0e9cbfb1b3676
Ivan Yurkiv <iyurkiv@outlook.com>
2023-09-16 13:52:06 -0500

    All routes working

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,75 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  let userswithsamename = users.filter((user) => {
+    return user.username === username
+  });
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 100000 * 100000 });
+
+    req.session.authorization = {
+      accessToken, username
+    }
+    req.session.myName = username;
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const text = req.body.review;
+  const isbn = req.params.isbn;
+  const user = req.session.myName;  // gets username from session object middleware
+  const book = books[isbn];
+  const reviews = book.reviews;
+
+  reviews[user] = text;
+
+  return res.status(300).json({ message: books });
+});
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const user = req.session.myName;  // gets username from session object middleware
+  const book = books[isbn];
+  const reviews = book.reviews;
+
+  delete reviews[user];
+  return res.status(300).json({ message: "Review posted by " + user + " has been deleted!" });
 });
 
 module.exports.authenticated = regd_users;
