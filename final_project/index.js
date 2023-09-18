4f23e30dd5ee999b50819500589fdb6977f88f15
Ivan Yurkiv <iyurkiv@outlook.com>
2023-09-18 16:33:55 -0500

    Promise rewrites doen

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const axios = require('axios');
 
 const app = express();
 
@@ -33,3 +34,5 @@ app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
+
+
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,14 +1,44 @@
 const express = require('express');
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+// promise code
+function getBooks(){
+  return new Promise((resolve, reject) => {
+    resolve(books);
+  });
+}
+
+function getBookByISBN(isbn){
+  return new Promise((resolve, reject) => {
+    resolve(books[isbn]);
+  });
+}
+
+function getBookByAuthor(author){
+  return new Promise((resolve, reject) => {
+    const result = (Object.values(books)).filter((book) => book.author == author)
+    resolve(result);
+  });
+}
+
+function getBookByTitle(title){
+  return new Promise((resolve, reject) => {
+    const result = (Object.values(books)).filter((book) => book.title == title)
+    resolve(result);
+  });
+}
+
+
+
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
-
+  
   if (username && password) {
     if (!isValid(username)) { 
       users.push({"username":username,"password":password});
@@ -21,27 +51,40 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books,null,1));
-  //return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  // res.send(JSON.stringify(books,null,1));
+
+  getBooks().then((sucessMessage) => {
+    res.send(JSON.stringify(sucessMessage,null,1));
+  });
+
 });
+
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-    return res.send(books[isbn]);
-  //return res.status(300).json({message: "Yet to be implemented"});
+  // return res.send(books[isbn]);
+  getBookByISBN(isbn).then((sucessMessage) => {
+    res.send(JSON.stringify(sucessMessage,null,1));
+  });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
-  const result = (Object.values(books)).filter((book) => book.author == author);
+  // const result = (Object.values(books)).filter((book) => book.author == author);
+  // return res.send(JSON.stringify(result,null,1));
 
-  return res.send(JSON.stringify(result,null,1));
+
+  // promise rewrite
+  getBookByAuthor(author).then((sucessMessage) => {
+    res.send(JSON.stringify(sucessMessage,null,1));
+  });
+
 
 });
 
@@ -49,9 +92,15 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
-  const result = (Object.values(books)).filter((book) => book.title == title);
+  // const result = (Object.values(books)).filter((book) => book.title == title);
 
-  return res.send(JSON.stringify(result,null,1));
+  // return res.send(JSON.stringify(result,null,1));
+
+  // promise rewrite
+  getBookByTitle(title).then((sucessMessage) => {
+    res.send(JSON.stringify(sucessMessage,null,1));
+  });
+
 });
 
 //  Get book review
